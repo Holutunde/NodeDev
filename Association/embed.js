d1bda561ef8cd38e202f8ab5a0c51b994c28c361
Holutunde <Holutunde99@gmail.com>
2022-03-16 23:48:03 +0100

    embedding

--- Association/embed.js
+++ Association/embed.js
@@ -2,14 +2,6 @@ var mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/blog_demo')
 
-//USER - email, name
-var userSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-})
-
-var User = mongoose.model('User', userSchema)
-
 //POST - title, content
 var postSchema = new mongoose.Schema({
   title: String,
@@ -18,9 +10,23 @@ var postSchema = new mongoose.Schema({
 
 var Post = mongoose.model('Post', postSchema)
 
+//USER - email, name
+var userSchema = new mongoose.Schema({
+  email: String,
+  name: String,
+  posts: [postSchema],
+})
+
+var User = mongoose.model('User', userSchema)
+
 var newUser = new User({
-  email: 'Charlie@gmail.com',
-  name: 'Charlie Brown',
+  email: 'Holushola@gmail.com',
+  name: 'Tunde Sokunbi',
+})
+
+newUser.posts.push({
+  title: 'How to beome big',
+  content: 'She want to do lowkey',
 })
 
 newUser.save(function (err, user) {
@@ -31,18 +37,18 @@ newUser.save(function (err, user) {
   }
 })
 
-var newPost = new Post({
-  title: 'Reflections on Apples',
-  content: 'They are delicious',
-})
+// var newPost = new Post({
+//     title: "This is a new post!",
+//     content: "Can you believe I just added this?",
+// });
 
-newPost.save(function (err, post) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(post)
-  }
-})
+// newPost.save(function(err, post) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
 
 // User.create({ email: 'Charlie@gmail.com', name: 'Charlie Brownn' }, function (
 //   err,
@@ -54,3 +60,22 @@ newPost.save(function (err, post) {
 //     console.log(user)
 //   }
 // })
+
+User.findOne({ name: 'Tunde Sokunbi' }, function (err, user) {
+  if (err) {
+    // console.log(err)
+  } else {
+    console.log(user)
+    user.posts.push({
+      title: '3 Things I really hate',
+      content: 'Voldmort, Voldmort, Voldmort',
+    })
+    user.save(function (err, user) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(user)
+      }
+    })
+  }
+})
