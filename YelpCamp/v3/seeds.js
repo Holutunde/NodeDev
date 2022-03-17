9a2f282ef51b7b4189d84e583d3128c78632f3f7
Holutunde <Holutunde99@gmail.com>
2022-03-17 20:26:04 +0100

    Added Comment to Seed

--- YelpCamp/v3/seeds.js
+++ YelpCamp/v3/seeds.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose')
+
 var Campground = require('./models/campground')
+var Comment = require('./models/comment')
 
 var data = [
   {
@@ -36,11 +38,27 @@ const seedDB = () => {
       console.log('All campgrounds removed')
       //Add a few campgrounds
       data.forEach(function (seed) {
-        Campground.create(seed, function (err, data) {
+        Campground.create(seed, function (err, campground) {
           if (err) {
             console.log(err)
           } else {
             console.log('Added a campground')
+            //Create a comment
+            Comment.create(
+              {
+                text: 'This place is dope',
+                author: 'Tommy Shelby',
+              },
+              function (err, comment) {
+                if (err) {
+                  console.log(err)
+                } else {
+                  campground.comments.push(comment)
+                  campground.save()
+                  console.log('Created New Comments')
+                }
+              },
+            )
           }
         })
       })
