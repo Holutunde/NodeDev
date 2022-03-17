0fc04da916cd07625c95fbd008e0d5a07f6c4434
Holutunde <Holutunde99@gmail.com>
2022-03-17 02:15:10 +0100

    Add Models for reusable

--- Association/references.js
+++ Association/references.js
@@ -1,26 +1,8 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/blog_demo_2')
-//POST - title, content
-var postSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-})
 
-var Post = mongoose.model('Post', postSchema)
-
-//USER - email, name
-var userSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-  posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Post',
-    },
-  ],
-})
-
-var User = mongoose.model('User', userSchema)
+var Post = require('./models/post')
+var User = require('./models/user')
 
 Post.create(
   {
