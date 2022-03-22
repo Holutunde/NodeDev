ee6be599e12ae74f399be3abaf9b54d4073314b9
Holutunde <Holutunde99@gmail.com>
2022-03-22 13:01:29 +0100

    YelpCamp Routes Refactoring

---
+++ YelpCamp/v7/routes/comments.js
@@ -0,0 +1,49 @@
+var express = require('express'),
+  router = express.Router({ mergeParams: true }),
+  Campground = require('../models/campground'),
+  Comment = require('../models/comment')
+
+//COMMENTS ROUTE
+
+//Comments New
+router.get('/new', isLoggedIn, function (req, res) {
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('comments/new', { campground: campground })
+    }
+  })
+})
+
+//Comments Create
+router.post('/', isLoggedIn, function (req, res) {
+  //lookup campground using id
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err)
+      res.redirect('/campgrounds')
+    } else {
+      Comment.create(req.body.comment, function (err, comment) {
+        if (err) {
+          console.log(err)
+        } else {
+          campground.comments.push(comment)
+          campground.save()
+          res.redirect('/campgrounds/' + campground._id)
+        }
+      })
+    }
+  })
+})
+
+//middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+module.exports = router
---
+++ YelpCamp/v7/seeds.js
@@ -0,0 +1,69 @@
+var mongoose = require('mongoose')
+
+var Campground = require('./models/campground')
+var Comment = require('./models/comment')
+
+var data = [
+  {
+    name: 'Thomas Edission',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgz2Bsvc_-JTfljr-Wjgrss9YWrxVZpJ8o-w&usqp=CAU',
+    description: 'You are doing well',
+  },
+  {
+    name: 'Steven Gerrard',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgz2Bsvc_-JTfljr-Wjgrss9YWrxVZpJ8o-w&usqp=CAU',
+    description: 'You are amazing',
+  },
+  {
+    name: 'Frank Lampard',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgz2Bsvc_-JTfljr-Wjgrss9YWrxVZpJ8o-w&usqp=CAU',
+    description: 'You are doing great',
+  },
+  {
+    name: 'Thierry Henry',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgz2Bsvc_-JTfljr-Wjgrss9YWrxVZpJ8o-w&usqp=CAU',
+    description: 'You are doing extra ordinary',
+  },
+]
+const seedDB = () => {
+  //Remove all campgronds
+  Campground.deleteMany({}, function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('All campgrounds removed')
+      //Add a few campgrounds
+      data.forEach(function (seed) {
+        Campground.create(seed, function (err, campground) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('Added a campground')
+            //Create a comment
+            Comment.create(
+              {
+                text: 'This place is dope',
+                author: 'Tommy Shelby',
+              },
+              function (err, comment) {
+                if (err) {
+                  console.log(err)
+                } else {
+                  campground.comments.push(comment)
+                  campground.save()
+                  console.log('Created New Comments')
+                }
+              },
+            )
+          }
+        })
+      })
+    }
+  })
+}
+
+module.exports = seedDB
