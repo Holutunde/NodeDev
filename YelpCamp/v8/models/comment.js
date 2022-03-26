be8365fb31ab7049f499dc8ed4fb527f62c2d3d8
Holutunde <Holutunde99@gmail.com>
2022-03-26 23:48:30 +0100

    YelpCamp: Username from login as Author in comments

---
+++ YelpCamp/v8/models/comment.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+
+//COMMENT - text, author
+var commentSchema = new mongoose.Schema({
+  text: String,
+  author: {
+    //storing the login details here
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      //ref - the model we refer to with the object ID
+      ref: 'User',
+    },
+    username: String,
+  },
+})
+
+module.exports = mongoose.model('Comment', commentSchema)
---
+++ YelpCamp/v8/routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express'),
+  passport = require('passport'),
+  router = express.Router({ mergeParams: true }),
+  User = require('../models/user')
+
+//root route
+router.get('/landing', function (req, res) {
+  res.render('landing')
+})
+//AUTH ROUTE
+//show register form
+router.get('/register', function (req, res) {
+  res.render('register')
+})
+router.post('/register', function (req, res) {
+  var user = req.body.username
+  var pass = req.body.password
+  User.register(new User({ username: user }), pass, function (err, user) {
+    if (err) {
+      console.log(err)
+      return res.render('register')
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      res.redirect('/campgrounds')
+    })
+  })
+})
+//show login form
+router.get('/login', function (req, res) {
+  res.render('login')
+})
+
+router.post(
+  '/login',
+  //middleware
+  passport.authenticate('local', {
+    successRedirect: '/campgrounds',
+    failureRedirect: '/login',
+  }),
+  function (req, res) {},
+)
+//Logout route
+router.get('/logout', function (req, res) {
+  req.logout()
+  res.redirect('/')
+})
+
+//midlleware
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+module.exports = router
