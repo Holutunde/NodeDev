ec46d2126f31f15df8c5dc0fac862150886ca7fd
Holutunde <Holutunde99@gmail.com>
2022-03-31 01:28:35 +0100

    YelpCamp: Comment Edit and Update

--- YelpCamp/v10/routes/comments.js
+++ YelpCamp/v10/routes/comments.js
@@ -16,7 +16,7 @@ router.get('/new', isLoggedIn, function (req, res) {
   })
 })
 
-//Comments Create
+//COMMENTS Create
 router.post('/', isLoggedIn, function (req, res) {
   //lookup campground using id
   Campground.findById(req.params.id, function (err, campground) {
@@ -43,27 +43,22 @@ router.post('/', isLoggedIn, function (req, res) {
 })
 
 //COMMENT EDIT
-router.get('/:comment_id/edit', function (req, res) {
+router.get('/:comment_id/edit', checkCommentOwnership, function (req, res) {
   Comment.findById(req.params.comment_id, function (err, foundComment) {
-    if (err) {
-      res.redirect('back')
-    } else {
-      res.render('comments/edit', {
-        campground_id: req.params.id,
-        comment: foundComment,
-      })
-    }
+    res.render('comments/edit', {
+      campground_id: req.params.id,
+      comment: foundComment,
+    })
   })
 })
+
 //COMMENT UPDATE
 router.put('/:comment_id', function (req, res) {
-  //find the campground with provided id
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (
     err,
     updatedComment,
   ) {
     if (err) {
-      console.log(err)
       res.redirect('back')
     } else {
       res.redirect('/campgrounds/' + req.params.id)
@@ -73,7 +68,7 @@ router.put('/:comment_id', function (req, res) {
 
 //DELETE
 router.delete('/:id', function (req, res) {
-  Campground.findById(req.params.id, function (err, campground) {
+  Comment.findById(req.params.id, function (err, campground) {
     if (err) {
       console.log(err)
     } else {
@@ -82,12 +77,34 @@ router.delete('/:id', function (req, res) {
     }
   })
 })
+
 //middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next()
   } else {
     res.redirect('/login')
+  }
+}
+
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+      if (err) {
+        res.redirect('back')
+      } else {
+        // does user own the comment?
+        if (foundComment.author.id.equals(req.user._id)) {
+          next()
+        } else {
+          req.flash('error', "You don't have permission to do that")
+          res.redirect('back')
+        }
+      }
+    })
+  } else {
+    req.flash('error', 'You need to be logged in to do that')
+    res.redirect('back')
   }
 }
 
