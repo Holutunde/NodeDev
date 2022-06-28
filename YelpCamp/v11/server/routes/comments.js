2ccd8ebbc809619c9e604031b64480515a287751
Holutunde <Holutunde99@gmail.com>
2022-06-28 11:16:15 +0100

    Added comment controller

--- YelpCamp/v11/server/routes/comments.js
+++ YelpCamp/v11/server/routes/comments.js
@@ -1,93 +1,28 @@
 var express = require('express'),
   router = express.Router({ mergeParams: true }),
-  Campground = require('../models/campground'),
-  Comment = require('../models/comment'),
   middleware = require('../../middleware/index')
 
-//COMMENTS ROUTE
+const {
+  createNewComment,
+  newCommentPage,
+  editComment,
+  updateComment,
+  deleteComment,
+} = require('../controllers/campground.js')
 
-//Comments New
-router.get('/new', middleware.isLoggedIn, function (req, res) {
-  Campground.findById(req.params.id, function (err, campground) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.render('comments/new', { campground: campground })
-    }
-  })
-})
+//GET NEW COMMENT PAGE
+router.get('/new', middleware.isLoggedIn, newCommentPage)
 
-//COMMENTS Create
-router.post('/', middleware.isLoggedIn, function (req, res) {
-  //lookup campground using id
-  Campground.findById(req.params.id, function (err, campground) {
-    if (err) {
-      console.log(err)
-      res.redirect('/campgrounds')
-    } else {
-      Comment.create(req.body.comment, function (err, comment) {
-        if (err) {
-          console.log(err)
-        } else {
-          //Add username and id to comment
-          comment.author.id = req.user._id
-          comment.author.username = req.user.username
-          //save comment
-          comment.save()
-          campground.comments.push(comment)
-          campground.save()
-          req.flash('success', 'Successfully added comment')
-          res.redirect('/campgrounds/' + campground._id)
-        }
-      })
-    }
-  })
-})
+//POST NEW COMMENT
+router.post('/', middleware.isLoggedIn, createNewComment)
 
 //COMMENT EDIT
-router.get('/:comment_id/edit', middleware.checkCommentOwnership, function (
-  req,
-  res,
-) {
-  Comment.findById(req.params.comment_id, function (err, foundComment) {
-    res.render('comments/edit', {
-      campground_id: req.params.id,
-      comment: foundComment,
-    })
-  })
-})
+router.get('/:comment_id/edit', middleware.checkCommentOwnership, editComment)
 
 //COMMENT UPDATE
-router.put('/:comment_id', middleware.checkCommentOwnership, function (
-  req,
-  res,
-) {
-  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (
-    err,
-    updatedComment,
-  ) {
-    if (err) {
-      res.redirect('back')
-    } else {
-      res.redirect('/campgrounds/' + req.params.id)
-    }
-  })
-})
+router.put('/:comment_id', middleware.checkCommentOwnership, updateComment)
 
-//DELETE
-router.delete('/:comment_id', middleware.checkCommentOwnership, function (
-  req,
-  res,
-) {
-  //findByIdAndRemove
-  Comment.findByIdAndRemove(req.params.comment_id, function (err) {
-    if (err) {
-      res.redirect('back')
-    } else {
-      req.flash('success', 'Comment deleted')
-      res.redirect('/campgrounds/' + req.params.id)
-    }
-  })
-})
+//COMMENT DELETE
+router.delete('/:comment_id', middleware.checkCommentOwnership, deleteComment)
 
 module.exports = router
